ea6a338ef05bbc20834181ae8d561d81f00818c4
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-11-25 20:25:05 +0000

    added products starter views, templates & urls

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Product, Category, Colour
 
 # Create your views here.
+
+def all_products(request):
+
+    """
+    To display all products, or products filter dependent.
+    """
+    products = Product.objects.all()
+
+    context = {
+        "products": products,
+    }
+
+    return render(request, "products/products.html", context)
