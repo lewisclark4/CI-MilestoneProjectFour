23c33aa95ad1b3091a7130f1c39b6ddde706f4dc
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-11-28 17:18:34 +0000

    added product_details view, URL & tested the template.

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category, Colour
 
 # Create your views here.
@@ -15,3 +15,18 @@ def all_products(request):
     }
 
     return render(request, "products/products.html", context)
+
+
+def product_detail(request, product_id):
+
+    """ 
+    returns the product detail view with all of the products information
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        "product": product,
+    }
+
+    return render(request, "products/product_detail.html", context)
