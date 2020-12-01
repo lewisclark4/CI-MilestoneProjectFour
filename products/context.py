03dda3dc4d66a4e9a5c19216acba2e8e43a8553d
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-12-01 20:49:28 +0000

    Added featured to product model and created featured-products.html include

--- products/context.py
+++ products/context.py
@@ -1,10 +1,20 @@
-from .models import Category
+from .models import Category, Product
 
 def all_categories(request):
     all_categories = Category.objects.all()
     
     context = {
         "all_categories": all_categories,
+    }
+
+    return context
+
+
+def featured_products(request):
+    featured_products = Product.objects.all().filter(featured=True)
+    
+    context = {
+        "featured_products": featured_products,
     }
 
     return context
