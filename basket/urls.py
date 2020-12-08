bcc8b0678e4189afad0a05d184e77eb00d253b00
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-12-08 13:06:47 +0000

    updated basket views, context & url to pass colour of the product into the basket

--- basket/urls.py
+++ basket/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.view_basket, name="view_basket"),
-    path("add/<product_id>/", views.add_to_basket, name="add_to_basket"),
+    path("add/<product_id>/<colour_id>", views.add_to_basket, name="add_to_basket"),    
 ]
+
--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from products.models import Product
+from products.models import Product, Colour
 
 # Create your views here.
 
@@ -8,18 +8,20 @@ def view_basket(request):
     return render(request, "basket/basket.html")
 
 
-def add_to_basket(request, product_id):
+def add_to_basket(request, product_id, colour_id=None):
     """Add products to the basket"""
 
     product = get_object_or_404(Product, pk=product_id)
+    colour_id = request.POST.get('colour')
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
 
-    if product_id in list(basket.keys()):
-        basket[product_id] += quantity
+    if colour_id in list(basket.keys()):
+        basket[colour_id] += quantity
     else:
-        basket[product_id] = quantity
+        basket[colour_id] = quantity
 
     request.session['basket'] = basket
+    print(basket)
     return redirect(redirect_url)
