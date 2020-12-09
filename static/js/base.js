c171ecc8b8bb6272ae013fafb768baef8d5389e1
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-12-09 22:55:41 +0000

    added increment & decrement buttons and js functions + minor styling

--- static/js/base.js
+++ static/js/base.js
@@ -1,6 +1,7 @@
 // Wait for site to load before initiating js
 $(document).ready(function () {
     
+
     $('#scroll-top').click(function () {
         window.scrollTo(0, 0);
     });
@@ -12,4 +13,27 @@ $(document).ready(function () {
             $('.scroll-top-button').fadeOut(500);
         }
     };
+
+    $('.increment-qty').click(function incrementQty() {
+        let productId = $(this).data('item_id')
+        let input = document.getElementById(`id_qty_${productId}`);
+        let currentVal = parseInt(input.value);
+        let maxVal = parseInt(input.getAttribute("max"));
+        if (input.value < maxVal) {
+        input.value = currentVal + 1;
+        } else {
+        input.value = maxVal;
+      }
+    })
+
+    $('.decrement-qty').click(function decrementQty() {
+        let productId = $(this).data('item_id')
+        let input = document.getElementById(`id_qty_${productId}`);
+        let currentVal = parseInt(input.value);
+        if (input.value >= 2) {
+        input.value = currentVal - 1;
+        } else {
+        input.value = 1;
+        }
+    })
 });
