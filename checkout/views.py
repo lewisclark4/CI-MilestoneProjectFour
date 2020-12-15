5fe6cb10a804ca3c764b8c651265e582e06ae47b
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-12-15 14:26:18 +0000

    updated checkout view to iterate through basket and pass items to stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
-from django.http import JsonResponse
 from .forms import OrderForm
 from products.models import Product, Colour
 import stripe
-
-
-
 
 
 def checkout(request):
@@ -16,32 +12,57 @@ def checkout(request):
     STRIPE_PUBLISHABLE = settings.STRIPE_PUBLISHABLE
     STRIPE_SUCCESS_URL = settings.STRIPE_SUCCESS_URL 
     STRIPE_CANCEL_URL = settings.STRIPE_CANCEL_URL
-    STRIPE_CURRENCY = settings.STRIPE_CURRENCY
+    STRIPE_CURRENCY = settings.STRIPE_CURRENCY 
+    DELIVERY = settings.STANDARD_DELIVERY_CHARGE
+    order_form = OrderForm()
 
     if not basket:
         messages.error(request, "There's nothing in your basket at the moment")
         return redirect(reverse('products'))
-
     
+    line_items = []
+    total = 0
+    for colour_id, item_data in basket.items():
+        colour = get_object_or_404(Colour, pk=colour_id)
+        product  = get_object_or_404(Product, pk=colour.product.id)
+        name = product.product_name + ' - ' + colour.colour
+        image = product.image_url
+        price = product.price
+        total += item_data * product.price
+        product_count = item_data
+        line_items.append(
+           {'price_data': {
+                'currency': STRIPE_CURRENCY,
+                'product_data': {
+                    'name': name,
+                    'images': [image],
+                },
+                'unit_amount': int(price*100),
+            },
+        'quantity': item_data,
+        })
+
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        line_items.append(
+            {'price_data': {
+                    'currency': STRIPE_CURRENCY,
+                    'product_data': {
+                        'name': 'Delivery',
+                    },
+                    'unit_amount': int(DELIVERY*100),
+            },
+        'quantity': 1,
+        })
 
     session = stripe.checkout.Session.create(
         payment_method_types=['card'],
-        line_items=[{
-        'price_data': {
-            'currency': STRIPE_CURRENCY,
-            'product_data': {
-            'name': 'T-shirt',
-            },
-            'unit_amount': 4000,
-        },
-        'quantity': 2,
-        }],
+        line_items=line_items,
         mode='payment',
         success_url=request.build_absolute_uri(reverse('view_basket')) +'?session_id={CHECKOUT_SESSION_ID}',
         cancel_url=request.build_absolute_uri(reverse('checkout')),
     )
 
-    order_form = OrderForm()
     context = {
         'session_id' : session.id,
         'stripe_public_key' : STRIPE_PUBLISHABLE,
@@ -50,13 +71,6 @@ def checkout(request):
 
     return render(request, 'checkout/checkout.html', context,)
 
-  
-    """
-    return JsonResponse({
-        'session_id' : session.id,
-        'stripe_secret_key' : stripe.api_key,
-    })
-    """
 
 
 
