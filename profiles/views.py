e2b7223afab1023e24539363555ce4db36728d94
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-12-19 15:59:56 +0000

    created profile view + URL and added site styling to allauth templates

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import UserProfile
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def profile(request):
+    
+    return render(request, "profiles/profile.html")
