dca2c14403c2186926496eb27757452f698972d7
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-12-19 20:32:50 +0000

    added UserProfileForm & update view to enable user to update/amend their delivery details

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
-from .models import UserProfile
-
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import UserProfile
+from .forms import UserProfileForm
+
 
 
 @login_required
 def profile(request):
+
+    profile = get_object_or_404(UserProfile, user=request.user)
+    form = UserProfileForm(instance=profile)
     
-    return render(request, "profiles/profile.html")
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your delivery details were updated successfully')
+
+    context = {
+        'form': form,
+    }
+    
+    return render(request, "profiles/profile.html", context)
