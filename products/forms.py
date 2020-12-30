0b626706857b965e9284232a249cfbe162909bcc
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-12-30 18:27:51 +0000

    added forms tests in products app and home app views test

--- products/forms.py
+++ products/forms.py
@@ -6,7 +6,7 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = '__all__'
+        exclude = ('slug',)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
