7b3035be1302da41b419c170beff6d5f260692b0
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-12-31 20:17:17 +0000

    added forms & views tests for checkout app

---
+++ checkout/tests/test_forms.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from checkout.forms import OrderForm
+
+class TestCheckoutForms(TestCase):
+    def test_checkout_form_explicit_fields(self):
+        form = OrderForm()
+        self.assertEqual(
+            form.Meta.fields,
+            (
+                'full_name',
+                'email',
+                'phone_number',
+                'address_1',
+                'address_2',
+                'city',
+                'county',
+                'country',
+                'postcode', 
+            ),
+        )
+
+    def test_checkout_form_required_fields(self):
+        form = OrderForm(
+            {
+                'full_name': '',
+                'email': '',
+                'phone_number': '',
+                'address_1': '',
+                'city': '',
+                'country': '',
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['full_name'][0], 'This field is required.')
+        self.assertEqual(form.errors['email'][0], 'This field is required.')
+        self.assertEqual(form.errors['phone_number'][0], 'This field is required.')
+        self.assertEqual(form.errors['city'][0], 'This field is required.')
+        self.assertEqual(form.errors['country'][0], 'This field is required.')
