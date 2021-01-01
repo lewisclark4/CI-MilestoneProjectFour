4c2e895bfba705132dba8a0a77de488ad95c71bd
lewisclark4 <lewis-clark@hotmail.co.uk>
2021-01-01 17:07:20 +0000

    pep8 formatting + update search view to search on category friendly name

--- search/tests/test_views.py
+++ search/tests/test_views.py
@@ -1,20 +1,19 @@
 from django.test import TestCase
 from django.contrib.messages import get_messages
-from products.models import Product, Category, Colour
+from products.models import Product, Category
 from django.db.models import Q
 
-# Create your tests here.
 
 class TestSearchViews(TestCase):
     def setUp(self):
 
         new_category = Category.objects.create(
-            name='test_category', 
-            slug='test-category', 
+            name='test_category',
+            slug='test-category',
             friendly_name='test category',
         )
 
-        new_product = Product.objects.create(
+        Product.objects.create(
             category=new_category,
             brand_name='test brand',
             product_name='test product',
@@ -22,23 +21,18 @@ class TestSearchViews(TestCase):
             price=1
             )
 
-        new_colour = Colour.objects.create(
-            product=new_product,
-            colour='test colour',
-            hex_value='000'
-        )
-
     def test_search_view(self):
         response = self.client.get('/search/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'search/search.html')
-   
+
     def test_search_by_product_name(self):
         url = '/search/?q=test+product'
         query = 'test product'
         response = self.client.get(url)
-        search_result = Product.objects.all().filter(Q(product_name__contains=query))
-        
+        search_result = Product.objects.all().filter(Q(
+                            product_name__contains=query))
+
         self.assertQuerysetEqual(
             response.context['products'], search_result, transform=lambda a: a)
 
@@ -46,8 +40,9 @@ class TestSearchViews(TestCase):
         url = '/search/?q=test+brand'
         query = 'test brand'
         response = self.client.get(url)
-        search_result = Product.objects.all().filter(Q(brand_name__contains=query))
-        
+        search_result = Product.objects.all().filter(Q(
+                            brand_name__contains=query))
+
         self.assertQuerysetEqual(
             response.context['products'], search_result, transform=lambda a: a)
 
@@ -55,20 +50,22 @@ class TestSearchViews(TestCase):
         url = '/search/?q=test+category'
         query = 'test category'
         response = self.client.get(url)
-        search_result = Product.objects.all().filter(Q(category__name__contains=query))
-        
+        search_result = Product.objects.all().filter(Q(
+                            category__friendly_name__contains=query))
+
         self.assertQuerysetEqual(
             response.context['products'], search_result, transform=lambda a: a)
-    
+
     def test_search_by_description(self):
         url = '/search/?q=test+description'
         query = 'test description'
         response = self.client.get(url)
-        search_result = Product.objects.all().filter(Q(description__contains=query))
-        
+        search_result = Product.objects.all().filter(Q(
+                            description__contains=query))
+
         self.assertQuerysetEqual(
             response.context['products'], search_result, transform=lambda a: a)
-    
+
     def test_error_message_blank_search(self):
         url = '/search/?q='
         response = self.client.get(url)
@@ -81,8 +78,6 @@ class TestSearchViews(TestCase):
     def tearDown(self):
         new_category = Category.objects.get(name='test_category')
         new_product = Product.objects.get(product_name='test product')
-        new_colour = Colour.objects.get(colour='test colour')
 
         del new_category
         del new_product
-        del new_colour
--- search/views.py
+++ search/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-from products.models import Category, Product
+from products.models import Product
 from django.db.models import Q
 
 
@@ -12,20 +12,20 @@ def search_result(request):
     products = None
     query = None
 
-    if "q" in request.GET:
-        query = request.GET["q"]
+    if 'q' in request.GET:
+        query = request.GET['q']
         if not query:
-            messages.warning(request, "Please enter a search criteria")
-            return redirect(reverse("products"))
+            messages.warning(request, 'Please enter a search criteria')
+            return redirect(reverse('products'))
         products = Product.objects.all().filter(
             Q(product_name__icontains=query)
             | Q(description__icontains=query)
             | Q(brand_name__icontains=query)
-            | Q(category__name__icontains=query)
+            | Q(category__friendly_name__icontains=query)
         )
 
     context = {
-        "query": query,
-        "products": products,
+        'query': query,
+        'products': products,
     }
-    return render(request, "search/search.html", context)
+    return render(request, 'search/search.html', context)
