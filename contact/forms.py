e4190ceede480a2b2bdffb3a92d2d95f1abf2df7
lewisclark4 <lewis-clark@hotmail.co.uk>
2021-01-01 16:49:49 +0000

    minor HTML updates + uploaded compressed category images

--- contact/forms.py
+++ contact/forms.py
@@ -16,7 +16,6 @@ class SubscriptionForm(forms.ModelForm):
         for field in self.fields:
             placeholder = placeholders[field]
             self.fields[field].widget.attrs["placeholder"] = placeholder
-            self.fields[field].label = False
 
 class SecureMessageForm(forms.ModelForm):
     class Meta:
