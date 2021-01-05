ae135c7d9e1591324fc40cef1afb628689dec389
lewisclark4 <lewis-clark@hotmail.co.uk>
2021-01-05 22:50:43 +0000

    connect to AWS to use s3 for static & media files storage

--- hex_cosmetics/settings.py
+++ hex_cosmetics/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'storages',
 
     #Custom
     'home',
@@ -51,7 +52,6 @@ INSTALLED_APPS = [
     'checkout',
     'profiles',
     'contact',
-
 
     # 3rd Party
     'crispy_forms',
@@ -179,6 +179,30 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if "USE_AWS" in os.environ:
+    # Cache static files
+    AWS_S3_OBJECT_PARAMETERS = {
+        "Expires": "Thu, 31 Dec 2099 20:00:00 GMT",
+        "CacheControl": "max-age=94608000",
+    }
+
+    # Bucket Configuration
+    AWS_STORAGE_BUCKET_NAME = 'hex-cosmetics'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID= os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Delivery values
 FREE_DELIVERY_THRESHOLD = 50
